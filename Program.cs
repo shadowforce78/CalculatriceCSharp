3e053b9caa1d53ce026b72937203e50d910a970d
shadowforce78 <planque.adam@gmail.com>
2025-02-11 21:26:33 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Calculatrice C# ===");
+
+        Console.Write("Premier nombre: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Opérateur (+, -, *, /): ");
+        char op = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        Console.Write("Deuxième nombre: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = op switch
+        {
+            '+' => num1 + num2,
+            '-' => num1 - num2,
+            '*' => num1 * num2,
+            '/' => num2 != 0 ? num1 / num2 : double.NaN,
+            _ => double.NaN
+        };
+
+        Console.WriteLine($"Résultat: {result}");
+    }
+}
